5e3bd4caab64c79489f3ec18ff6304085a6c4999
BingHawk <93518779+BingHawk@users.noreply.github.com>
2022-01-12 16:25:32 +0100

    5 correct

--- KommentarerMain.java
+++ KommentarerMain.java
@@ -1,10 +1,16 @@
 import java.io.IOException;
+import java.util.Scanner;
 
 public class KommentarerMain {
     public static Boolean inComment = false;
+    public static Boolean slashDebt = false; //true om vi har ett överhoppat / som bör skrivas ut.
     public static String commentType = null;
 
     public static void main(String[] args){
+        streaming();
+    }
+
+    public static void streaming(){
         int i;
 
         try {
@@ -13,58 +19,118 @@ public class KommentarerMain {
                 char c = (char)i;
 
                 if(c == '%'){
-                    System.out.print("[% detected]");
+                    paySlashDept(slashDebt);
                     parsePercent();
                 } else if (c == '*'){
-                    System.out.print("[* detected, prev = "+prev+"]");
                     parseStar(prev);
                 } else if (c == '/'){
-                    System.out.print("[/ detected, prev = "+prev+"]");
+                    paySlashDept(slashDebt);
                     parseSlash(prev);
                 } else {
                     if(!inComment){
+                        paySlashDept(slashDebt);
                         System.out.print(c);
                     }
                 }
                 prev = c;
             }
+            inComment = false;
+            commentType = null;
         }
         catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    public static void buffering(){
+        Scanner scan = new Scanner( System.in );
+        StringBuffer buf = new StringBuffer();
+        while ( scan.hasNextLine() ) {
+            buf.append( scan.nextLine() );
+            buf.append( "\n" );
+        }
+        scan.close();
+        String s = buf.toString();
+        //System.out.println("testar skriva s: "+s);
+
+        char prev = '0';
+        for (int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+
+            if(c == '%'){
+                parsePercent();
+            } else if (c == '*'){
+                parseStar(prev);
+            } else if (c == '/'){
+                parseSlash(prev);
+            } else {
+                if(!inComment){
+                    System.out.print(c);
+                }
+            }
+            prev = c;
+        }
+    }
     //Slår på eller av kommentarsläge om % kommer
     private static void parsePercent(){
+        //System.out.print("[%: inC: "+inComment+" , cType: "+commentType+"]");
         if(!inComment){
             inComment = true;
             commentType = "%";
         } else {
-            inComment = false;
-            commentType = null;
-            //TODO: lägg till hantering av annan kommentar
+            if(commentType == "%"){
+                inComment = false;
+                commentType = null;
+            }
         }
     }
 
     //Slår på kommentarsläge om /kommer före *
     private static void parseStar(char prev){
+        //System.out.print("[*: prev: "+prev+" , inC: "+inComment+" , cType: "+commentType+"]");
         if(!inComment){
             if(prev == '/'){
-                inComment = true;
-                commentType = "*/";
+                if(commentType != "%"){
+                    inComment = true;
+                    commentType = "*/";
+                    slashDebt = false; //Nollställer slash dept eftersom en * kom efter. 
+                }
+            } else {
+                System.out.print('*');
             }
         }
     }
 
     //stännger av komentarsläge om stjärna kommer innan /
     private static void parseSlash(char prev){
+        //System.out.print("[/: prev: "+prev+" , inC: "+inComment+" , cType: "+commentType+"]");
         if(inComment){
             if(prev == '*'){
-                inComment = false;
-                commentType = null;
-                //TODO: hantera annan kommentarstyp
+                if(commentType == "*/"){
+                    inComment = false;
+                    commentType = null;
+                }
             }
+        } else {
+            slashDebt = true; //Vi ha ett / att skriva ut om nästa inte är en *
+        }
+    }
+
+    //skriver ut / om vi har hoppat över ett som skulle skrivas ut
+    public static void paySlashDept(boolean dept){
+        if (dept){
+            System.out.print('/');
+            slashDebt = false;
         }
     }
 }
 
+/*
+
+Charles knocked on the door and a woman 
+opened it. % Don't forget that Charles 
+has already met this woman in chapter 5! % 
+She looked at him.
+- Yes?, she said.
+
+*/
---
+++ kommentarer5.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+class kommentarer5 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner( System.in );
+        StringBuffer buf = new StringBuffer();
+        while ( scan.hasNextLine() ) {
+            buf.append( scan.nextLine() );
+            buf.append( "\n" );
+        }
+        String text = buf.toString(); 
+
+        // TESTSTRÄNG
+        //String text = "Charles knocked on the door and a woman opened it. % Hmm, is this good... /* Not sure to 100% */ Perhaps this should happen in chapter 10 instead? % She looked at him.- Yes?, she said.";
+        String storage = "0";
+
+        while (text.contains("%") || text.contains("/*")) {
+
+            String[] parts;
+
+            int plats1 = text.indexOf("%");
+            int plats2 = text.indexOf("/*");
+            //System.out.println(plats1 + " and " + plats2);
+
+            if (plats1 == -1) plats1=plats2+1;          // "placerar" /* en plats bakom % (för att gå in i rätt if-sats)
+            else if (plats2 == -1) plats2=plats1+1;     // Tvärtom
+
+            //System.out.print("% finns pa plats: " + plats1 + ". ");
+            //System.out.print("/* finns pa plats: " + plats2 + ". ");
+
+            // 1--------------> Om % före /* <---------------
+            if (plats1 < plats2) {
+                parts = text.split("%", 2);
+                storage = storage + parts[0];
+                if (parts[1].contains("%")) {   // Om det finns kommentarsavslut
+                    text=parts[1];
+                    parts = text.split("%", 2);
+                    text=parts[1];
+                }
+                else text = "";
+            }
+
+            else if (plats1 > plats2) {
+                String text2;
+                storage = storage + text.substring(0,plats2);    // Substring för att lösa fallet /*/*/*/
+                text2 = text.substring(plats2+2);
+                if (text2.contains("*/")) {  // Om det finns kommentarsavslut
+                    //parts1 = storage + text.substring(0,plats2);    // Substring för att lösa fallet /*/*/*/
+                    int plats3 = text2.indexOf("*/");
+                    text = text2.substring(plats3+2);
+                    //System.out.println(plats3 + " and " + text);
+                }
+                else text="";
+            }
+        //System.out.println(storage + " and " + text);
+        }
+        storage=storage+text;
+        storage=storage.substring(1);
+        System.out.println(storage);
+    }
+}
